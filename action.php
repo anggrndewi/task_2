a6e662a2d996e1b66dddf4835cf4f21d750e17f1
anggrndewi <dewi.120140149@student.itera.ac.id>
2022-10-06 19:18:40 +0700

    Pendaftaran

---
+++ action.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Informasi Detail</title>
+    <link rel="stylesheet" href="style.css">
+
+    <style>
+        table{
+            position: absolute;
+            left: 50%;
+            top: 40%;
+            transform: translate(-50%, -50%);
+            padding: 25px 25px;
+            width: 1200px;
+            background-color: rgba(235, 173, 211, 0.7);
+            box-shadow: 0 0 10px rgba(255,255,255,.3);
+            border-radius: 10px;
+            border: 3px solid black;
+        }
+    </style>
+</head>
+<body>
+    <h2><b><p>Data Pendaftar</p></b></h2>
+    <table border="1">
+        <tr>
+            <td>Nama</td>
+            <td>Jenis Kelamin</td>
+            <td>Alamat</td>
+            <td>Tanggal Lahir</td>
+            <td>Pendidikan Terakhir</td>
+            <td>Kelas yang akan diambil</td>
+            <td>Agama</td>
+            <td>Nomor Telepon</td>
+            <td>Email</td>
+        </tr>
+
+        <tr>
+            <td>Dewi Anggraini</td>
+            <td>Perempuan</td>
+            <td>Bandar Lampung</td>
+            <td>10/06/2003</td>
+            <td>SMA</td>
+            <td>Web Program</td>
+            <td>Islam</td>
+            <td>0853xx2345</td>
+            <td>anggrn@gmail.com</td>
+        </tr>
+    </table>
+</body>
+</html>
